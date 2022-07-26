aba829385ddf6486e53cc154efe366ac6a19448c
minsoo.lee <letomok@gmail.com>
2022-07-26 21:36:40 +0900

    Initial Commit

---
+++ war-challenge-tutoriala/war_challenge_tutorialaApp.swift
@@ -0,0 +1,17 @@
+//
+//  war_challenge_tutorialaApp.swift
+//  war-challenge-tutoriala
+//
+//  Created by MinsooLee on 2022/07/26.
+//
+
+import SwiftUI
+
+@main
+struct war_challenge_tutorialaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
