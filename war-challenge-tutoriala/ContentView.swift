9dd8978526e57b21c7ad67d32a22b6f493480f87
minsoo.lee <letomok@gmail.com>
2022-07-26 22:24:47 +0900

    layout 추가

--- war-challenge-tutoriala/ContentView.swift
+++ war-challenge-tutoriala/ContentView.swift
@@ -9,8 +9,75 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        
+        ZStack {
+            Spacer()
+            Image("background")
+                .resizable()
+                .scaledToFill().ignoresSafeArea()
+            
+            VStack {
+                Spacer()
+                
+                Image("logo")
+                
+                Spacer()
+                
+                
+                HStack {
+                    Spacer()
+                    Image("card3")
+                    Spacer()
+                    Image("card4")
+                    Spacer()
+                }
+                
+                Spacer()
+                
+                Image("dealbutton")
+                
+                Spacer()
+                
+                
+                HStack {
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 10.0) {
+                        Text("Player").foregroundColor(Color.white)
+                        Text("0").foregroundColor(Color.white)
+                    }
+                    
+                    Spacer()
+                    
+                    VStack(spacing: 10.0) {
+                        Text("CPU").foregroundColor(Color.white)
+                        Text("0").foregroundColor(Color.white)
+                    }
+                    
+                    Spacer()
+                }
+                
+
+                
+                Spacer()
+                
+                
+                
+            }
+            
+            
+            
+        }
+        
+
+        
+
+        
+        
+        
+    
+        
     }
 }
 
