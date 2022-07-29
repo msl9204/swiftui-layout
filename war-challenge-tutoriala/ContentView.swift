6cfd305ab44db4221245a1aa2dee1df65e21f6d1
minsoo.lee <letomok@gmail.com>
2022-07-29 17:18:50 +0900

    model, viewmodel 추가

--- war-challenge-tutoriala/ContentView.swift
+++ war-challenge-tutoriala/ContentView.swift
@@ -8,6 +8,13 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    
+    @State private var playerCard = "card5"
+    @State private var cpuCard = "card9"
+    @State private var playerScore = 0
+    @State private var cpuScore = 0
+    
     var body: some View {
         
         ZStack {
@@ -26,15 +33,39 @@ struct ContentView: View {
                 
                 HStack {
                     Spacer()
-                    Image("card3")
+                    Image(playerCard)
                     Spacer()
-                    Image("card4")
+                    Image(cpuCard)
                     Spacer()
                 }
                 
                 Spacer()
                 
-                Image("dealbutton")
+                
+                Button(action: {
+                    
+                    // generate random number between 2 and 14
+                    let playerRand = Int.random(in: 2...14)
+                    let cpuRand = Int.random(in: 2...14)
+                    
+                    
+                    // Update the cards
+                    playerCard = "card" + String(playerRand)
+                    cpuCard = "card" + String(cpuRand)
+                    
+                    
+                    // Update the score
+                    // playerScore += 1
+                    // cpuScore += 1
+                    if playerRand > cpuRand {
+                        playerScore += 1
+                    } else if playerRand < cpuRand {
+                        cpuScore += 1
+                    }
+                    
+                }, label: {
+                    Image("dealbutton")
+                })
                 
                 Spacer()
                 
@@ -44,40 +75,31 @@ struct ContentView: View {
                     Spacer()
                     
                     VStack(spacing: 10.0) {
-                        Text("Player").foregroundColor(Color.white)
-                        Text("0").foregroundColor(Color.white)
+                        Text("Player")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                        Text(String(playerScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
                     }
                     
                     Spacer()
                     
                     VStack(spacing: 10.0) {
-                        Text("CPU").foregroundColor(Color.white)
-                        Text("0").foregroundColor(Color.white)
+                        Text("CPU")
+                            .font(.headline)
+                            .foregroundColor(Color.white)
+                        Text(String(cpuScore))
+                            .font(.largeTitle)
+                            .foregroundColor(Color.white)
                     }
                     
                     Spacer()
                 }
-                
-
-                
                 Spacer()
-                
-                
-                
+
             }
-            
-            
-            
         }
-        
-
-        
-
-        
-        
-        
-    
-        
     }
 }
 
